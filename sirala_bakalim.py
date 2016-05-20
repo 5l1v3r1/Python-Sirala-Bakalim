bde96586a22243c909229a62fcc9213cb400e895
İSMAİL TAŞDELEN <pentestdatabase@gmail.com>
2016-05-20 02:37:14 -0400

    Add files via upload

---
+++ sirala_bakalim.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+sirala_bakalim_ico = """
+#########################################################
+#       PYTHON - SIRALA BAKALIM - GH0ST S0FTWARE        #
+######################################################### 
+#                       CONTACT                         #
+#########################################################
+#              DEVELOPER : İSMAİL TAŞDELEN              #                       
+#           GMAIL : pentestdatabase@gmail.com           #
+# Linkedin : https://www.linkedin.com/in/ismailtasdelen #
+#           Whatsapp : + 90 534 295 94 31               #
+#########################################################
+"""
+
+star = "#########################################################"
+
+print sirala_bakalim_ico
+
+print star
+
+ilk_sayi = input("İlk sayıyı belirleyiniz :")
+son_sayi = input("İkinci sayiyi belirleyiniz :")
+
+print star
+
+for sayi in range(ilk_sayi, son_sayi):
+	print(sayi)
+
+print son_sayi
+print star
